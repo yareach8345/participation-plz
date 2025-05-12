56f6a57073c21781941bb4a6456ce6bbfa85f00c
yareach8345 <mine8345@naver.com>
2025-05-13 02:56:02 +0900

    feat: 스타일 수정
    - 선택과 관련된 스타일 선언
    - inner 형식의 그림자 선언
    - 1px로 외곽만 나타내는 border-thin 선언

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,20 @@ export default <Partial<Config>>{
                 'n-bg': '#ECF0F3',
                 'n-light-shadow': '#FFF',
                 'n-dark-shadow': '#D1D9E6',
+                yes: '#4CAF50',
+                no: '#EF5350',
             },
             backgroundColor: {
-                neumorphism: 'theme("colors.n-bg")'
+                neumorphism: 'theme("colors.n-bg")',
+                yes: '#B9F6CA',
+                no: '#FFCDD2',
             },
             boxShadow: {
-                neumorphism: '0.625rem 0.625rem 0.875rem 0 theme("colors.n-dark-shadow"), -0.5rem -0.5rem 1.125rem 0 theme("colors.n-light-shadow")'
+                neumorphism: '0.625rem 0.625rem 0.875rem 0 theme("colors.n-dark-shadow"), -0.5rem -0.5rem 1.125rem 0 theme("colors.n-light-shadow")',
+                'neumorphism-inner': 'inset 0.625rem 0.625rem 0.875rem 0 theme("colors.n-dark-shadow"), -0.5rem -0.5rem 1.125rem 0 theme("colors.n-light-shadow")'
+            },
+            borderWidth: {
+                'thin': '1px'
             }
         }
     },

4eff2a9f8fbcf7a453e48a060c08b8a602df23ef
yareach8345 <mine8345@naver.com>
2025-05-14 01:45:42 +0900

    feat: Timer추가
    보고 마감 시간까지 남은 시간을 보여주는 Timer를 추가

---
+++ components/Timer.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+interface Time {
+  hours: number
+  minutes: number
+  seconds: number
+}
+
+interface Props {
+  gatheringDay?: boolean
+  gatheringTime: Date
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  gatheringDay: true,
+})
+
+const currentTime = ref(new Date())
+
+if(import.meta.client) {
+  const handler = setInterval(() => {
+    currentTime.value = new Date()
+  }, 1000)
+
+  onUnmounted(() => {
+    clearInterval(handler)
+  })
+}
+
+const time = computed(() => {
+  const sec = (props.gatheringTime.getTime() - currentTime.value.getTime()) / 1000
+  if (sec <= 0 || !props.gatheringDay) {
+    return {
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    }
+  } else {
+    const hours = Math.floor(sec / 60 / 60 % 24)
+    const minutes = Math.floor(sec / 60 % 60)
+    const seconds = Math.floor(sec % 60)
+    return { hours, minutes, seconds }
+  }
+})
+
+const timeString = computed(() => {
+  const hours = time.value.hours.toString().padStart(2, "0")
+  const minutes = time.value.minutes.toString().padStart(2, "0")
+  const seconds = time.value.seconds.toString().padStart(2, "0")
+  return `${hours}:${minutes}:${seconds}`
+})
+
+const timeColor = computed(() => {
+  if(time.value.hours === 0 && time.value.minutes === 0 && time.value.seconds === 0) {
+    return "text-red-900"
+  } if(time.value.hours === 0) {
+    return "text-red-500"
+  } if(time.value.hours < 3) {
+    return "text-red-500"
+  } else {
+    return ""
+  }
+})
+</script>
+
+<template>
+  <client-only>
+    <div :class="[timeColor]">{{timeString}}</div>
+  </client-only>
+</template>
+
+<style scoped>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-
+const data = new Date()
+data.setHours(18, 0, 0, 0)
 </script>
 
 <template>
@@ -8,6 +9,7 @@
       <h1 class="text-5xl pb-2">모임 참여 보고</h1>
       <div class="py-3">
         <div>보고는 빠를수록 은혜롭습니다.</div>
+        <timer :gathering-time="data"/>
       </div>
     </header>
     <slot></slot>

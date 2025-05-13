b6d44f5f051c47e9017f5eda4df2e1915fde5e6c
yareach8345 <mine8345@naver.com>
2025-05-14 00:25:23 +0900

    feat: VerseViewer추가
    - 성경 구절을 보여주는 목적의 VerseViewer추가
    - index페이지에서 상단에서 테스트로 구절 보여주고 있음

---
+++ components/VerseViewer.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+interface Props {
+  verse: string
+  content: string
+  description?: string | undefined
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <NeumorphismBox>
+    <div class="break-keep p-3">{{props.content}}</div>
+    <div><b>{{props.verse}}</b></div>
+    <template v-if="props.description !== undefined">
+      <hr class="border-thin m-1"/>
+      {{description}}
+    </template>
+  </NeumorphismBox>
+</template>
+
+<style scoped>
+
+</style>

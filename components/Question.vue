f7e96027293e481324a59d8ea4160f04e019f113
yareach8345 <mine8345@naver.com>
2025-05-13 23:49:29 +0900

    feat: Question 업데이트
    - 눌린 버튼에 따라 문장이 달리지게 함
    - 버튼이 눌려지면 배경색이 달라지게 함

--- components/Question.vue
+++ components/Question.vue
@@ -7,20 +7,50 @@ interface Props {
 }
 
 const {object, positivePredicate, negativePredicate, interrogative} = defineProps<Props>()
+
+const model = defineModel<boolean | undefined>({ default: undefined })
+
+const positiveBtnClicked = () => {
+  model.value = true
+}
+
+const negativeBtnClicked = () => {
+  model.value = false
+}
+
+const predicate = computed(() => {
+  switch (model.value) {
+    case undefined:
+      return interrogative
+    case true:
+      return positivePredicate
+    case false:
+      return negativePredicate
+  }
+})
+
+const predicateColor = computed(() => {
+  switch (model.value) {
+    case undefined:
+      return ""
+    case true:
+      return "text-yes"
+    case false:
+      return "text-no"
+  }
+})
 </script>
 
 <template>
   <div class="my-5 flex flex-col items-center">
     <slot name="icon"></slot>
-    <h2 class="text-2xl mt-3">{{ object }} {{ interrogative }}</h2>
+    <h2 class="text-2xl mt-3">{{ object }} <span :class="[predicateColor]">{{ predicate }}</span></h2>
   </div>
   <div class="flex justify-center">
     <custom-button
-        @click="() => {console.log('click')}"
-        @mouseenter="() => console.log('enter')"
-        @mouseleave="() => console.log('leave')"
-
+        @click="positiveBtnClicked"
         class="hover:bg-yes flex flex-col items-center w-[43%] sm:w-[33%] md:max-w-[30%]"
+        :class="{ 'shadow-neumorphism-inner': model, 'bg-yes': model }"
     >
       <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-check-big-icon lucide-circle-check-big stroke-yes"><path d="M21.801 10A10 10 0 1 1 17 3.335"/><path d="m9 11 3 3L22 4"/></svg>
       <div class="text-xl">
@@ -28,7 +58,9 @@ const {object, positivePredicate, negativePredicate, interrogative} = defineProp
       </div>
     </custom-button>
     <custom-button
+        @click="negativeBtnClicked"
         class="hover:bg-no flex flex-col items-center w-[43%] sm:w-[33%] md:max-w-[30%]"
+        :class="{ 'shadow-neumorphism-inner': model != undefined && !model, 'bg-no': model != undefined && !model }"
     >
       <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-ban-icon lucide-ban stroke-no"><circle cx="12" cy="12" r="10"/><path d="m4.9 4.9 14.2 14.2"/></svg>
       <div class="text-xl">
